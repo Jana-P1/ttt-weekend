2266aadd67289eaac9841ea71baa9ef56daf8ff2
Jana Pascual <jacpascual@gmail.com>
2022-02-04 17:04:33 -0500

    Sections 1 -4 complete

--- js/app.js
+++ js/app.js
@@ -1,6 +1,15 @@
 /*-------------------------------- Constants --------------------------------*/
 // Define an array to represent the 9 squares on the board
-
+const player1 = 1
+const player2 = -1
+winningCombos = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8]
+]
 
 
 /*---------------------------- Variables (state) ----------------------------*/
@@ -13,7 +22,7 @@ let nextTurn, isWinner, gameBoard
 
 
 /*------------------------ Cached Element References ------------------------*/
-let squareArr = document.querySelectorAll(".square")
+let squareArr = Array.from(document.querySelectorAll(".square"))
 console.log(squareArr)
 let messageInH2 = document.getElementById("message") 
 
@@ -26,43 +35,45 @@ let messageInH2 = document.getElementById("message")
 // Initialize Function
 
 function init() {
-  let gameBoard = [null, null, null, null, null, null, null, null, null];
-  const players = {
-    "1" : {
-      name: "",
-      score: 0
-    },
-    "-1": {
-      name: "",
-      score: 0
-    }
-  };
-  console.log(gameBoard);
+  gameBoard = [-1, null, null, null, null, null, null, null, null]
+  turn = 1
   winner = null;
   render();
 }
 init()
 
 function render() { 
-  // Reads through the squareArr array to 
-  for(let i = 0; i < squareArr.length; i++) {
-    if (squareArr[i] === 1) {
-      squareArr[i].innerHTML = "X"
-    } else if (squareArr[i] === -1) {
-      squareArr[i].innerHTML= "O"
-    } else (
-      squareArr[i] = null
-    )
-    console.log(squareArr[i])
-    return squareArr[i]
-    }
+  for(let i = 0; i < gameBoard.length; i++) {
+    // console.log(gameBoard[i], gameBoard, i)
+    if (gameBoard[i] === 1) {
+          gameBoard[i].textContent = "X"
+        } else if (gameBoard[i] === -1) {
+          gameBoard[i].textContent= "O"
+        } else if (gameBoard[i] === null) {
+          gameBoard[i].textContent = " "
+        }
+  }
   if (winner !== null){
     // Display message for next turn `Take a turn ${nextTurn}`
   } else if (winner === "T") {
     //  Display message for a tie "Well look at that! We got us a tie!"
-  } else{
+  } else {
     // Display message for winner `Congrats, ${winner}! You tic-tac-toe'd your way to a win!`
   }
 }
-
 render()
+
+function handleClick(_squareArr) {
+  // Get the index of the square that is selected
+  // Extract the index from an id assigned to the appropriate <div> in HTML 
+  // Return the id and index once selected, so that the other player cannot select the same square
+  // Change the player's turn by => (turn *= -1)
+  // Set winner variable if there is a winner
+    // Call getWinner function
+  console.log("handleclick invoked")
+}
+handleClick()
+
+function getWinner() {
+  console.log("getWinner invoked")
+}
